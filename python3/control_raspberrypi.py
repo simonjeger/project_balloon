dc707f6be165cbe05ccd3186487920fd8cc1ce24
simonjeger <simonjeger@gmail.com>
2021-12-06 16:06:19 +0000

    File of raspberry pi

--- python3/control_raspberrypi.py
+++ python3/control_raspberrypi.py
@@ -19,7 +19,7 @@ print("calibrate OR manual OR control OR arm OR stop")
 def manual_drive(): #You will use this function to program your ESC if required
     print("You have selected manual option so give a value between 0 and you max value")
     while True:
-        inp = raw_input()
+        inp = input()
         if inp == "stop":
             stop()
             break
@@ -35,11 +35,11 @@ def manual_drive(): #You will use this function to program your ESC if required
 def calibrate():   #This is the auto calibration procedure of a normal ESC
     pi.set_servo_pulsewidth(ESC, 0)
     print("Disconnect the battery and press Enter")
-    inp = raw_input()
+    inp = input()
     if inp == '':
         pi.set_servo_pulsewidth(ESC, max_value)
         print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
-        inp = raw_input()
+        inp = input()
         if inp == '':
             pi.set_servo_pulsewidth(ESC, min_value)
             pi.set_servo_pulsewidth(ESC, 0)
@@ -57,7 +57,7 @@ def control():
     print("Controls - a to decrease speed & d to increase speed OR q to decrease a lot of speed & e to increase a lot of speed")
     while True:
         pi.set_servo_pulsewidth(ESC, speed)
-        inp = raw_input()
+        inp = input()
 
         if inp == "q":
             speed -= 100    # decrementing the speed like hell
@@ -85,7 +85,7 @@ def control():
 
 def arm(): #This is the arming procedure of an ESC
     print("Connect the battery and press Enter")
-    inp = raw_input()
+    inp = input()
     if inp == '':
         pi.set_servo_pulsewidth(ESC, 0)
         time.sleep(1)
@@ -100,7 +100,7 @@ def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
     pi.stop()
 
 #This is the start of the program
-inp = raw_input()
+inp = input()
 if inp == "manual":
     manual_drive()
 elif inp == "calibrate":
