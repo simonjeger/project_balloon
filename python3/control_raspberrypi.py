70625c5bdbf748aa26adf7004e27d5d46f092f73
simonjeger <simonjeger@gmail.com>
2021-12-06 15:50:16 +0000

    File for raspberry pi

--- python3/control_raspberrypi.py
+++ python3/control_raspberrypi.py
@@ -1,276 +1,124 @@
-import rospy
-from geometry_msgs.msg import PoseStamped, Point
-from geometry_msgs.msg import TwistStamped
-from std_msgs.msg import Header
-import numpy as np
-import time
-import urllib.request
-import sys
-import select
-import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
-import pickle
-import os
-import time
-import json
+# This program will let you test your ESC and brushless motor.
+# Make sure your battery is not connected if you are going to calibrate it at first.
+# Since you are testing your motor, I hope you don't have your propeller attached to it otherwise you are in trouble my friend...?
+# This program is made by AGT @instructable.com. DO NOT REPUBLISH THIS PROGRAM... actually the program itself is harmful                                             pssst Its not, its safe.
 
-from build_ll_controller import ll_controler
-from utils.ekf import ekf
+import os     #importing os library so as to communicate with the system
+import time   #importing time library to make Rpi wait because its too impatient
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+import pigpio #importing GPIO library
 
-import yaml
-import argparse
+ESC=4  #Connect the ESC in this GPIO pin
 
-# Get yaml parameter
-parser = argparse.ArgumentParser()
-parser.add_argument('yaml_file')
-args = parser.parse_args()
-with open(args.yaml_file, 'rt') as fh:
-    yaml_p = yaml.safe_load(fh)
+pi = pigpio.pi();
+pi.set_servo_pulsewidth(ESC, 0) 
 
-class character_vicon():
-    def __init__(self):
-        self.position = [0,0,0]
-        self.velocity = [0,0,0]
+max_value = 2000 #change this if your ESC's max value is different or leave it be
+min_value = 700  #change this if your ESC's min value is different or leave it be
+print "For first time launch, select calibrate"
+print "Type the exact word for the function you want"
+print "calibrate OR manual OR control OR arm OR stop"
 
-        self.set_vicon()
-        #print(self.position)
+def manual_drive(): #You will use this function to program your ESC if required
+    print "You have selected manual option so give a value between 0 and you max value"
+    while True:
+        inp = raw_input()
+        if inp == "stop":
+            stop()
+            break
+		elif inp == "control":
+			control()
+			break
+		elif inp == "arm":
+			arm()
+			break
+        else:
+            pi.set_servo_pulsewidth(ESC,inp)
 
-    def set_vicon(self):
-        rospy.init_node('listener', anonymous=True)
-        rospy.Subscriber("/vrpn_client_node/balloon/pose", PoseStamped, self.callback_pos)
-        rospy.Subscriber("/vrpn_client_node/balloon/twist", TwistStamped, self.callback_vel)
+def calibrate():   #This is the auto calibration procedure of a normal ESC
+    pi.set_servo_pulsewidth(ESC, 0)
+    print("Disconnect the battery and press Enter")
+    inp = raw_input()
+    if inp == '':
+        pi.set_servo_pulsewidth(ESC, max_value)
+        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        inp = raw_input()
+        if inp == '':
+            pi.set_servo_pulsewidth(ESC, min_value)
+            print "Wierd eh! Special tone"
+            time.sleep(7)
+            print "Wait for it ...."
+            time.sleep (5)
+            print "Im working on it, DONT WORRY JUST WAIT....."
+            pi.set_servo_pulsewidth(ESC, 0)
+            time.sleep(2)
+            print "Arming ESC now..."
+            pi.set_servo_pulsewidth(ESC, min_value)
+            time.sleep(1)
+            print "See.... uhhhhh"
+            control() # You can change this to any other function you want
 
-        r = rospy.Rate(10)
-        r.sleep()
+def control():
+    print "I'm Starting the motor, I hope its calibrated and armed, if not restart by giving 'x'"
+    time.sleep(1)
+    speed = 1500    # change your speed if you want to.... it should be between 700 - 2000
+    print "Controls - a to decrease speed & d to increase speed OR q to decrease a lot of speed & e to increase a lot of speed"
+    while True:
+        pi.set_servo_pulsewidth(ESC, speed)
+        inp = raw_input()
 
-    def callback_pos(self, data):
-        offset_vicon_x = 1.25
-        offset_vicon_y = 0.75
-        offset_vicon_z = -0.725 #+0.28
+        if inp == "q":
+            speed -= 100    # decrementing the speed like hell
+            print "speed = %d" % speed
+        elif inp == "e":
+            speed += 100    # incrementing the speed like hell
+            print "speed = %d" % speed
+        elif inp == "d":
+            speed += 10     # incrementing the speed
+            print "speed = %d" % speed
+        elif inp == "a":
+            speed -= 10     # decrementing the speed
+            print "speed = %d" % speed
+        elif inp == "stop":
+            stop()          #going for the stop function
+            break
+        elif inp == "manual":
+            manual_drive()
+            break
+		elif inp == "arm":
+			arm()
+			break
+        else:
+            print "WHAT DID I SAID!! Press a,q,d or e"
 
-
-        self.position[0] = data.pose.position.x + offset_vicon_x
-        #self.position[0] = 0 + offset_vicon_x #to turn into 2d problem
-        self.position[1] = data.pose.position.y + offset_vicon_y
-        self.position[2] = data.pose.position.z + offset_vicon_z
-
-    def callback_vel(self, data):
-        self.velocity[0] = data.twist.linear.x
-        self.velocity[1] = data.twist.linear.y
-        self.velocity[2] = data.twist.linear.z
-
-def call(con):
-    url = "http://192.168.0.238/"  + str(con)
-    n = urllib.request.urlopen(url).read() # get the raw html data in bytes (sends request and warn our esp8266)
-    n = n.decode("utf-8") # convert raw html bytes format to string :3
-
-    # data = n
-    data = n.split() 			#<optional> split datas we got. (if you programmed it to send more than one value) It splits them into seperate list elements.
-    #data = list(map(int, data)) #<optional> turn datas to integers, now all list elements are integers.
-    return data
-
-def send(data):
-    path = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/'
-    with open(path + 'data.txt', 'w') as f:
-        f.write(json.dumps(data))
-    return data
-
-def receive():
-    successful = False
-    path = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/'
-    start = time.time()
-    corrupt = False
-    while not successful:
-        with open(path + 'action.txt') as json_file:
-            try:
-                data = json.load(json_file)
-                successful = True
-            except:
-                corrupt = True
-    if corrupt:
-        print('data corrupted, lag of ' + str(np.round(time.time() - start,3)) + '[s]')
-    return data
-
-def update_est(position,u,c,delta_t):
-    est_x.one_cycle(0,position[0],c,delta_t)
-    est_y.one_cycle(0,position[1],c,delta_t)
-    est_z.one_cycle(u,position[2],c,delta_t)
-    position_est = [est_x.xhat_0[0], est_y.xhat_0[0], est_z.xhat_0[0]]
-    return position_est
-
-def plot_pid(plot_time, plot_pos_z, plot_vel, plot_u, plot_action):
-    plt.plot(plot_time, plot_pos_z)
-    plt.plot(plot_time, plot_vel)
-    plt.plot(plot_time, plot_u)
-    plt.plot(plot_time, plot_action)
-    plt.xlabel('[s]')
-    plt.ylabel('[m]')
-    plt.title('max velocity: ' + str(np.round(np.max(plot_vel),2)) + 'm/s')
-    plt.savefig('debug_pid.png')
-    plt.close()
-
-def plot(plot_pos_x, plot_pos_y, plot_pos_z, target, plot_residual):
-    # loading file
-    filepath = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/3dplot.obj'
-    file = open(filepath, 'wb')
-    if os.path.isfile(filepath):
-        ax = pickle.load(filepath)
-    else:
-        ax = plt.axes(projection='3d')
-
-    # plotting
-    ax.plot3D(plot_pos_x, plot_pos_y, plot_pos_z)
-    ax.plot3D(np.linspace(0,yaml_p['size_x']*yaml_p['unit_xy'], 10), [target[1]*yaml_p['unit_xy']]*10, [target[2]*yaml_p['unit_z']]*10)
-
-    # mark the border of the box
-    ax.set_xlim3d(0, yaml_p['size_x']*yaml_p['unit_xy'])
-    ax.set_ylim3d(0, yaml_p['size_y']*yaml_p['unit_xy'])
-    ax.set_zlim3d(0, yaml_p['size_z']*yaml_p['unit_z'])
-
-    ax.set_title('min 2d residual: ' + str(np.round(np.linalg.norm(plot_residual),3)) + ' m')
-
-    file = open(filepath, 'wb')
-    pickle.dump(ax,file)
-    plt.show()
-    plt.close()
-
-def tuning(delta_t):
-    if delta_t < 2:
-        action = 0
-    elif delta_t < 22:
-        action = 0.8
-    elif delta_t < 40:
-        action = 0.2
-    else:
-        action = -1
-    return action
-
-character = character_vicon()
-offset = 0.16
-scale = 0.7
-
-llc = ll_controler()
-
-est_x = ekf(character.position[0])
-est_y = ekf(character.position[1])
-est_z = ekf(character.position[2])
-
-path = []
-path_est = []
-not_done = True
-
-plot_time = []
-plot_pos_x = []
-plot_pos_y = []
-plot_pos_z = []
-plot_vel = []
-plot_u = []
-plot_action = []
-plot_residual = np.inf
-
-U = 0
-min_proj_dist = np.inf
-
-global_start = time.time()
-while True:
-    start = time.time()
-
-    if not os.path.isfile(yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/action.txt'):
+def arm(): #This is the arming procedure of an ESC
+    print "Connect the battery and press Enter"
+    inp = raw_input()
+    if inp == '':
+        pi.set_servo_pulsewidth(ESC, 0)
         time.sleep(1)
-        print('waiting for the algorithm to publish')
+        pi.set_servo_pulsewidth(ESC, max_value)
+        time.sleep(1)
+        pi.set_servo_pulsewidth(ESC, min_value)
+        time.sleep(1)
+        control()
 
-        data = {
-        'U': U,
-        'position': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
-        'velocity': np.divide(character.velocity,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
-        'path': [],
-        'position_est': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
-        'path_est': [],
-        'measurement': [0, 0],
-        'min_proj_dist': 0,
-        'not_done': not_done}
+def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
+    pi.set_servo_pulsewidth(ESC, 0)
+    pi.stop()
 
-    else:
-        data = receive()
-        action = data['action']
-        target = data['target']
-        ceiling = data['ceiling']
-        c = data['c']
-
-        character.set_vicon()
-        terrain = 0
-
-        rel_pos = (character.position[2] - terrain)/(ceiling-terrain)
-        rel_vel = character.velocity[2] / (ceiling-terrain)
-
-        # check if done or not
-        if (character.position[0] < 0) | (character.position[0]/yaml_p['unit_xy'] > yaml_p['size_x'] - 1):
-            print('x out of bounds')
-            not_done = False
-        if (character.position[1] < 0) | (character.position[1]/yaml_p['unit_xy'] > yaml_p['size_y'] - 1):
-            print('y out of bounds')
-            not_done = False
-        #if (rel_pos < 0) | (rel_pos >= 1):
-        #    print('z out of bounds')
-        #    not_done = False
-        #if self.t < 0: #check if flight time is over
-        #    not_done = False
-        #if self.battery_level < 0: #check if battery is empty
-        #    not_done = False
-
-        #action = tuning(time.time() - global_start)
-        u_raw = llc.pid(action, rel_pos, rel_vel)
-        if yaml_p['mode'] == 'tuning':
-            print(u_raw)
-        u = offset + u_raw*(1-offset)*scale
-
-        call(u)
-        if (not not_done) | (action < 0):
-            u = 0
-            call(u)
-            plot_pid(plot_time, plot_pos_z, plot_vel, plot_u, plot_action)
-            #plot(plot_pos_x, plot_pos_y, plot_pos_z, target, plot_residual)
-
-        stop = time.time()
-        delta_t = stop - start
-
-        position_est = np.divide(update_est(character.position,u,c,delta_t),[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']])
-
-        path.append(np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist())
-        path_est.append(np.divide(position_est,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist())
-
-        plot_pos_x.append(character.position[0])
-        plot_pos_y.append(character.position[1])
-        plot_pos_z.append(character.position[2])
-        plot_vel.append(character.velocity[2])
-        plot_u.append(u)
-        plot_action.append(action*ceiling)
-        plot_time.append(time.time())
-
-        U += abs(u*delta_t)
-
-        # find min_proj_dist
-        render_ratio = yaml_p['unit_xy']/yaml_p['unit_z']
-        residual = target - np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']])
-        min_proj_dist_prop = np.sqrt((residual[1]*render_ratio/yaml_p['radius_xy'])**2 + (residual[2]/yaml_p['radius_z'])**2) #only 2d case!
-        min_dist_prop = np.sqrt((residual[1]*render_ratio)**2 + (residual[2])**2)*yaml_p['unit_z']
-        if min_proj_dist_prop < min_proj_dist:
-            min_proj_dist = min_proj_dist_prop
-            min_dist = min_dist_prop
-            plot_residual = target*np.array([yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]) - character.position
-
-        data = {
-        'U': U,
-        'position': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
-        'velocity': np.divide(character.velocity,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
-        'path': path,
-        'position_est': position_est.tolist(),
-        'path_est': path_est,
-        'measurement': [est_x.wind(), est_y.wind()],
-        'min_proj_dist': min_proj_dist,
-        'min_dist': min_dist,
-        'not_done': not_done}
-
-    send(data)
-    if (not not_done) | (action < 0):
-        break
+#This is the start of the program actually, to start the function it needs to be initialized before calling... stupid python.
+inp = raw_input()
+if inp == "manual":
+    manual_drive()
+elif inp == "calibrate":
+    calibrate()
+elif inp == "arm":
+    arm()
+elif inp == "control":
+    control()
+elif inp == "stop":
+    stop()
+else :
+    print "Thank You for not following the things I'm saying... now you gotta restart the program STUPID!!"
--- python3/control_vicon.py
+++ python3/control_vicon.py
@@ -1,11 +1,276 @@
-from gpiozero import Servo
+import rospy
+from geometry_msgs.msg import PoseStamped, Point
+from geometry_msgs.msg import TwistStamped
+from std_msgs.msg import Header
+import numpy as np
 import time
+import urllib.request
+import sys
+import select
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+import pickle
+import os
+import time
+import json
 
-servo = Servo(32)
+from build_ll_controller import ll_controler
+from utils.ekf import ekf
 
-servo.min()
-time.sleep(3)
-servo.mid()
-time.sleep(3)
-servo.max()
-time.sleep(3)
+import yaml
+import argparse
+
+# Get yaml parameter
+parser = argparse.ArgumentParser()
+parser.add_argument('yaml_file')
+args = parser.parse_args()
+with open(args.yaml_file, 'rt') as fh:
+    yaml_p = yaml.safe_load(fh)
+
+class character_vicon():
+    def __init__(self):
+        self.position = [0,0,0]
+        self.velocity = [0,0,0]
+
+        self.set_vicon()
+        #print(self.position)
+
+    def set_vicon(self):
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber("/vrpn_client_node/balloon/pose", PoseStamped, self.callback_pos)
+        rospy.Subscriber("/vrpn_client_node/balloon/twist", TwistStamped, self.callback_vel)
+
+        r = rospy.Rate(10)
+        r.sleep()
+
+    def callback_pos(self, data):
+        offset_vicon_x = 1.25
+        offset_vicon_y = 0.75
+        offset_vicon_z = -0.725 #+0.28
+
+
+        self.position[0] = data.pose.position.x + offset_vicon_x
+        #self.position[0] = 0 + offset_vicon_x #to turn into 2d problem
+        self.position[1] = data.pose.position.y + offset_vicon_y
+        self.position[2] = data.pose.position.z + offset_vicon_z
+
+    def callback_vel(self, data):
+        self.velocity[0] = data.twist.linear.x
+        self.velocity[1] = data.twist.linear.y
+        self.velocity[2] = data.twist.linear.z
+
+def call(con):
+    url = "http://192.168.0.238/"  + str(con)
+    n = urllib.request.urlopen(url).read() # get the raw html data in bytes (sends request and warn our esp8266)
+    n = n.decode("utf-8") # convert raw html bytes format to string :3
+
+    # data = n
+    data = n.split() 			#<optional> split datas we got. (if you programmed it to send more than one value) It splits them into seperate list elements.
+    #data = list(map(int, data)) #<optional> turn datas to integers, now all list elements are integers.
+    return data
+
+def send(data):
+    path = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/'
+    with open(path + 'data.txt', 'w') as f:
+        f.write(json.dumps(data))
+    return data
+
+def receive():
+    successful = False
+    path = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/'
+    start = time.time()
+    corrupt = False
+    while not successful:
+        with open(path + 'action.txt') as json_file:
+            try:
+                data = json.load(json_file)
+                successful = True
+            except:
+                corrupt = True
+    if corrupt:
+        print('data corrupted, lag of ' + str(np.round(time.time() - start,3)) + '[s]')
+    return data
+
+def update_est(position,u,c,delta_t):
+    est_x.one_cycle(0,position[0],c,delta_t)
+    est_y.one_cycle(0,position[1],c,delta_t)
+    est_z.one_cycle(u,position[2],c,delta_t)
+    position_est = [est_x.xhat_0[0], est_y.xhat_0[0], est_z.xhat_0[0]]
+    return position_est
+
+def plot_pid(plot_time, plot_pos_z, plot_vel, plot_u, plot_action):
+    plt.plot(plot_time, plot_pos_z)
+    plt.plot(plot_time, plot_vel)
+    plt.plot(plot_time, plot_u)
+    plt.plot(plot_time, plot_action)
+    plt.xlabel('[s]')
+    plt.ylabel('[m]')
+    plt.title('max velocity: ' + str(np.round(np.max(plot_vel),2)) + 'm/s')
+    plt.savefig('debug_pid.png')
+    plt.close()
+
+def plot(plot_pos_x, plot_pos_y, plot_pos_z, target, plot_residual):
+    # loading file
+    filepath = yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/3dplot.obj'
+    file = open(filepath, 'wb')
+    if os.path.isfile(filepath):
+        ax = pickle.load(filepath)
+    else:
+        ax = plt.axes(projection='3d')
+
+    # plotting
+    ax.plot3D(plot_pos_x, plot_pos_y, plot_pos_z)
+    ax.plot3D(np.linspace(0,yaml_p['size_x']*yaml_p['unit_xy'], 10), [target[1]*yaml_p['unit_xy']]*10, [target[2]*yaml_p['unit_z']]*10)
+
+    # mark the border of the box
+    ax.set_xlim3d(0, yaml_p['size_x']*yaml_p['unit_xy'])
+    ax.set_ylim3d(0, yaml_p['size_y']*yaml_p['unit_xy'])
+    ax.set_zlim3d(0, yaml_p['size_z']*yaml_p['unit_z'])
+
+    ax.set_title('min 2d residual: ' + str(np.round(np.linalg.norm(plot_residual),3)) + ' m')
+
+    file = open(filepath, 'wb')
+    pickle.dump(ax,file)
+    plt.show()
+    plt.close()
+
+def tuning(delta_t):
+    if delta_t < 2:
+        action = 0
+    elif delta_t < 22:
+        action = 0.8
+    elif delta_t < 40:
+        action = 0.2
+    else:
+        action = -1
+    return action
+
+character = character_vicon()
+offset = 0.16
+scale = 0.7
+
+llc = ll_controler()
+
+est_x = ekf(character.position[0])
+est_y = ekf(character.position[1])
+est_z = ekf(character.position[2])
+
+path = []
+path_est = []
+not_done = True
+
+plot_time = []
+plot_pos_x = []
+plot_pos_y = []
+plot_pos_z = []
+plot_vel = []
+plot_u = []
+plot_action = []
+plot_residual = np.inf
+
+U = 0
+min_proj_dist = np.inf
+
+global_start = time.time()
+while True:
+    start = time.time()
+
+    if not os.path.isfile(yaml_p['process_path'] + 'process' + str(yaml_p['process_nr']).zfill(5) + '/communication/action.txt'):
+        time.sleep(1)
+        print('waiting for the algorithm to publish')
+
+        data = {
+        'U': U,
+        'position': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
+        'velocity': np.divide(character.velocity,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
+        'path': [],
+        'position_est': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
+        'path_est': [],
+        'measurement': [0, 0],
+        'min_proj_dist': 0,
+        'not_done': not_done}
+
+    else:
+        data = receive()
+        action = data['action']
+        target = data['target']
+        ceiling = data['ceiling']
+        c = data['c']
+
+        character.set_vicon()
+        terrain = 0
+
+        rel_pos = (character.position[2] - terrain)/(ceiling-terrain)
+        rel_vel = character.velocity[2] / (ceiling-terrain)
+
+        # check if done or not
+        if (character.position[0] < 0) | (character.position[0]/yaml_p['unit_xy'] > yaml_p['size_x'] - 1):
+            print('x out of bounds')
+            not_done = False
+        if (character.position[1] < 0) | (character.position[1]/yaml_p['unit_xy'] > yaml_p['size_y'] - 1):
+            print('y out of bounds')
+            not_done = False
+        #if (rel_pos < 0) | (rel_pos >= 1):
+        #    print('z out of bounds')
+        #    not_done = False
+        #if self.t < 0: #check if flight time is over
+        #    not_done = False
+        #if self.battery_level < 0: #check if battery is empty
+        #    not_done = False
+
+        #action = tuning(time.time() - global_start)
+        u_raw = llc.pid(action, rel_pos, rel_vel)
+        if yaml_p['mode'] == 'tuning':
+            print(u_raw)
+        u = offset + u_raw*(1-offset)*scale
+
+        call(u)
+        if (not not_done) | (action < 0):
+            u = 0
+            call(u)
+            plot_pid(plot_time, plot_pos_z, plot_vel, plot_u, plot_action)
+            #plot(plot_pos_x, plot_pos_y, plot_pos_z, target, plot_residual)
+
+        stop = time.time()
+        delta_t = stop - start
+
+        position_est = np.divide(update_est(character.position,u,c,delta_t),[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']])
+
+        path.append(np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist())
+        path_est.append(np.divide(position_est,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist())
+
+        plot_pos_x.append(character.position[0])
+        plot_pos_y.append(character.position[1])
+        plot_pos_z.append(character.position[2])
+        plot_vel.append(character.velocity[2])
+        plot_u.append(u)
+        plot_action.append(action*ceiling)
+        plot_time.append(time.time())
+
+        U += abs(u*delta_t)
+
+        # find min_proj_dist
+        render_ratio = yaml_p['unit_xy']/yaml_p['unit_z']
+        residual = target - np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']])
+        min_proj_dist_prop = np.sqrt((residual[1]*render_ratio/yaml_p['radius_xy'])**2 + (residual[2]/yaml_p['radius_z'])**2) #only 2d case!
+        min_dist_prop = np.sqrt((residual[1]*render_ratio)**2 + (residual[2])**2)*yaml_p['unit_z']
+        if min_proj_dist_prop < min_proj_dist:
+            min_proj_dist = min_proj_dist_prop
+            min_dist = min_dist_prop
+            plot_residual = target*np.array([yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]) - character.position
+
+        data = {
+        'U': U,
+        'position': np.divide(character.position,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
+        'velocity': np.divide(character.velocity,[yaml_p['unit_xy'], yaml_p['unit_xy'], yaml_p['unit_z']]).tolist(),
+        'path': path,
+        'position_est': position_est.tolist(),
+        'path_est': path_est,
+        'measurement': [est_x.wind(), est_y.wind()],
+        'min_proj_dist': min_proj_dist,
+        'min_dist': min_dist,
+        'not_done': not_done}
+
+    send(data)
+    if (not not_done) | (action < 0):
+        break
