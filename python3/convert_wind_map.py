f88be9192ccd436e9c72537f30df4679265275f9
simonjeger <simonjeger@gmail.com>
2022-01-05 10:19:01 +0000

    Improved convert_wind_map.py

--- python3/convert_wind_map.py
+++ python3/convert_wind_map.py
@@ -121,20 +121,26 @@ def build_set(num, n_h, train_or_test):
     Path(yaml_p['data_path'] + train_or_test + '/tensor').mkdir(parents=True, exist_ok=True)
 
     seed_overall = np.random.randint(0,2**32 - 1)
+    num = int(num/4) #because the dataset will be 4 times bigger through the mirroring process
+
+    # read in first dataset for dimensions
+    tensor = torch.load(yaml_p['process_path'] + 'data_cosmo/tensor/wind_map_CH_' + str(0).zfill(2) + '.pt')
+    size_c = len(tensor)
+    size_x = yaml_p['size_x']
+    size_y = yaml_p['size_y']
+    size_z = yaml_p['size_z']
+    global_size_x = len(tensor[0])
+    global_size_y = len(tensor[0][0])
+    global_size_z = len(tensor[0][0][0])
+
+    # generate center coordinates
+    IDX_x = np.random.randint(0,global_size_x - size_x - 1,num*10) #just in case that some of them lie in the mountains
+    IDX_y = np.random.randint(0,global_size_y - size_y - 1,num*10) #just in case that some of them lie in the mountains
+
     for h in range(n_h):
         tensor = torch.load(yaml_p['process_path'] + 'data_cosmo/tensor/wind_map_CH_' + str(h).zfill(2) + '.pt')
         coord = torch.load(yaml_p['process_path'] + 'data_cosmo/coord/coord_map_CH_' + str(h).zfill(2) + '.pt')
-        size_c = len(tensor)
-        size_x = yaml_p['size_x']
-        size_y = yaml_p['size_y']
-        size_z = yaml_p['size_z']
 
-        global_size_x = len(tensor[0])
-        global_size_y = len(tensor[0][0])
-        global_size_z = len(tensor[0][0][0])
-
-        seed = seed_overall
-        N = int(num/4)
         for o in range(4):
             if o == 0:
                 tensor_rot = tensor[:,:,:,:]
@@ -148,32 +154,32 @@ def build_set(num, n_h, train_or_test):
                 tensor_rot = tensor[:,::-1,::-1,:]
                 tensor_rot[-4:-2,:,:,:] = -tensor_rot[-4:-2,:,:,:]
 
-            for n in range(N):
-                world = np.ones(shape=(1+4,size_x,size_y,size_z))*size_z #so that it makes the first "while" for sure
-                flat = 3
-                center_x = int(len(world[0])/2-flat/2)
-                center_y = int(len(world[0][0])/2-flat/2)
-                while np.max(world[0][center_x:center_x+flat,center_y:center_y+flat,0]) > (1 - yaml_p['min_space'])*size_z: #only generate maps with enough space
-                    np.random.seed(seed)
-                    seed += 1
-                    idx_x = np.random.randint(0,global_size_x - size_x - 1)
-                    np.random.seed(seed)
-                    seed += 1
-                    idx_y = np.random.randint(0,global_size_y - size_y - 1)
+            world = np.ones(shape=(1+4,size_x,size_y,size_z))*size_z #so that it makes the first "while" for sure
+            flat = 3
+            center_x = int(len(world[0])/2-flat/2)
+            center_y = int(len(world[0][0])/2-flat/2)
+            n = 0
+            s = 0
+            while s < num:
+                idx_x = IDX_x[n]
+                idx_y = IDX_y[n]
+                n += 1
 
-                    world = tensor_rot[:,idx_x:idx_x+size_x, idx_y:idx_y+size_y,:]
-                    coord_center = coord[:,idx_x + int(size_x/2), idx_y + int(size_y/2)] #only save the center coordinate (lat,lon)
+                world = tensor_rot[:,idx_x:idx_x+size_x, idx_y:idx_y+size_y,:]
+                coord_center = coord[:,idx_x + int(size_x/2), idx_y + int(size_y/2)] #only save the center coordinate (lat,lon)
 
-                # naming convention
-                digits = 4
-                name_lat = str(int(np.round(coord_center[0],digits)*10**digits)).zfill(digits+2)
-                name_lon = str(int(np.round(coord_center[1],digits)*10**digits)).zfill(digits+2)
-                name_lat = name_lat[0:2] + '.' + name_lat[2::]
-                name_lon = name_lon[0:2] + '.' + name_lon[2::]
-                name = name_lat + '_' + name_lon + '_' + str(o) + '_' + str(h).zfill(2) + '.pt'
-                torch.save(world, yaml_p['data_path'] + train_or_test + '/tensor/' + name)
+                if np.max(world[0][center_x:center_x+flat,center_y:center_y+flat,0]) >= yaml_p['min_space']*size_z: #only generate maps with enough space
+                    # naming convention
+                    digits = 4
+                    name_lat = str(int(np.round(coord_center[0],digits)*10**digits)).zfill(digits+2)
+                    name_lon = str(int(np.round(coord_center[1],digits)*10**digits)).zfill(digits+2)
+                    name_lat = name_lat[0:2] + '.' + name_lat[2::]
+                    name_lon = name_lon[0:2] + '.' + name_lon[2::]
+                    name = name_lat + '_' + name_lon + '_' + str(o) + '_' + str(h).zfill(2) + '.pt'
+                    torch.save(world, yaml_p['data_path'] + train_or_test + '/tensor/' + name)
 
-                print('generated ' + str(o*N + n + 1) + ' of ' + str(num) + ' maps at ' + str(h).zfill(2) + ':00')
+                    print('generated ' + str(o*num + s + 1) + ' of ' + str(num*4) + ' maps at ' + str(h).zfill(2) + ':00')
+                    s += 1
 
 def visualize_real_data(dimension):
     # reading the nc file and creating Dataset
@@ -288,5 +294,5 @@ def visualize_real_data(dimension):
 #visualize_real_data('time')
 #convert_map()
 
-build_set(5000, 4, 'train')
-build_set(500, 4, 'test')
+#build_set(5000, 24, 'train')
+build_set(10, 24, 'test')
